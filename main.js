ad54eac7ea485e4716aa8640444e856cc00a3cef
sabihasomapti9 <157345204+sabihasomapti9@users.noreply.github.com>
2024-01-23 19:58:30 +0600

    Add files via upload

--- main.js
+++ main.js
@@ -1,37 +1,7 @@
 
-const emailInput = document.getElementById("email");
-const subscribeButton = document.getElementById("subscribe-button");
-
-subscribeButton.addEventListener("click", (event) => {
-  const email = emailInput.value;
-  
-  if (!validateEmail(email)) {
-    event.preventDefault(); // Prevent form submission
-    // Display error message
-  } else {
-    // Submit form or handle subscription logic
-  }
-});
 
 
-document.getElementById("subscribe-form").addEventListener("submit", (event) => {
-  event.preventDefault();
-
-  
-});
-
-
-
-const email = emailInput.value;
-
-if (!validateEmail(email)) {
-  // Display error message
-} else {
-  // Proceed with subscription
-}
-
-
-const container = document.querySelector('.container');
+const container = document.querySelector('.container-h');
 const lines = document.querySelectorAll('.line1, .line2');
 
 function adjustLayout() {
